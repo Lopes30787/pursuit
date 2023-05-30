f5c972ebb68e3ef1bf97d928f1cdba53c55aa24e
s3np41k1r1t0 <brunomendes@tecnico.ulisboa.pt>
2023-05-30 16:09:30 +0100

    remove random file

--- policies/RandomPolicy copy.py
+++
@@ -1,7 +0,0 @@
-from .AbstractPolicy import AbstractPolicy
-import numpy as np
-
-
-class GreedyPolicy(AbstractPolicy):
-    def __call__(self, observation, agent):
-        return self.env.action_space(agent).sample()
